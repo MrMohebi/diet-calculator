c22144804361cbe4d07c4f1af8d077fb45101694
MMMohebi <MMMohebi@outlook.com>
2024-03-11 15:16:18 +0330

    tdee

---
+++ server/api/tdee.ts
@@ -0,0 +1,38 @@
+export default defineEventHandler( async (event) => {
+    const query = getQuery(event)
+    console.log(query);
+    const validator =
+        query.hasOwnProperty("gender") &&
+        query.hasOwnProperty("age") &&
+        query.hasOwnProperty("height") &&
+        query.hasOwnProperty("weight")
+
+    if(!validator){
+        throw createError({
+            statusCode: 400,
+            statusMessage: 'gender and age and height and weight are required',
+        })
+    }
+
+    const age = query["age"]
+    const height = query["height"]
+    const weight = query["weight"]
+
+    let bmr
+    if(query["gender"] == 'male'){
+        bmr = 88.362 + (13.397 * weight) + (4.799 * height) - (5.677 * age)
+    }else{
+        bmr = 447.593 + (9.247 * weight) + (3.098 * height) - (4.330 * age)
+    }
+
+    return {
+        bmr: Math.round(bmr),
+        sedentary: Math.round(bmr * 1.2),
+        lightlyActive: Math.round(bmr * 1.375),
+        moderatelyActive: Math.round(bmr * 1.55),
+        veryActive: Math.round(bmr * 1.725),
+        superActive: Math.round(bmr * 1.9),
+    }
+})
+
+
