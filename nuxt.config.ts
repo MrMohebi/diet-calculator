460a15e35ea9cd594d3c11fa475f45d5ecd44d6e
MMMohebi <MMMohebi@outlook.com>
2024-03-12 15:05:28 +0330

    install primevue

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,6 +4,12 @@ export default defineNuxtConfig({
   runtimeConfig: {
     FDC_API_KEY: 'ZK28BIF4kIQ79MdkTs4Cau17wf7hLKow2TRaUgfs'
   },
+  modules: [
+    'nuxt-primevue'
+  ],
+  primevue: {
+    /* Options */
+  }
   storage: {
     db: {
       driver: 'fs',
