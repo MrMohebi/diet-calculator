96b53a1a42e8c4c10354484d77674d97eff59b16
MMMohebi <MMMohebi@outlook.com>
2024-03-12 15:05:37 +0330

    add common foods

---
+++ consts/foods.ts
@@ -0,0 +1,202 @@
+const x = [
+    {
+        fdc_id:746771,
+        en_name:"Orange",
+        fa_name:"پرتقال",
+    },
+    {
+        fdc_id:330458,
+        en_name:"coconut oil",
+        fa_name:"روغن نارگیل",
+    },
+    {
+        fdc_id:1750340,
+        en_name:"Apple with skin",
+        fa_name:"سیب درختی با پوست",
+    },
+    {
+        fdc_id:2644283,
+        en_name:"Lentils, dry",
+        fa_name:"عدس خام",
+    },
+    {
+        fdc_id:1105073,
+        en_name:"Bananas, overripe",
+        fa_name:"موز رسیده",
+    },
+    {
+        fdc_id:174270,
+        en_name:"Soybeans, mature seeds",
+        fa_name:"سویا اجیلی",
+    },
+    {
+        fdc_id:2344896,
+        en_name:"Potato, roasted",
+        fa_name:"سیب زمینی کبابی",
+    },
+    {
+        fdc_id:2344879,
+        en_name:"Potato, boiled",
+        fa_name:"سیب زمینی آبپز",
+    },
+    {
+        fdc_id:2512380,
+        en_name:"Rice, brown",
+        fa_name:"برنج قهوه ای",
+    },
+    {
+        fdc_id:2512381,
+        en_name:"Rice, white",
+        fa_name:"برنچ سفید",
+    },
+    {
+        fdc_id:747445,
+        en_name:"Beans, pinto",
+        fa_name:"لوبیا چیتی",
+    },
+    {
+        fdc_id:747440,
+        en_name:"Beans, Dark Red Kidney",
+        fa_name:"لوبیا قرمز",
+    },
+    {
+        fdc_id:2003591,
+        en_name:"Orange juice, no pulp",
+        fa_name:"آب پرتقال",
+    },
+    {
+        fdc_id:171320,
+        en_name:"Cinnamon",
+        fa_name:"دارچین",
+    },
+    {
+        fdc_id:2346396,
+        en_name:"Oats, whole grain, rolled",
+        fa_name:"جو پرک",
+    },
+    {
+        fdc_id:2346396,
+        en_name:"Chia seeds",
+        fa_name:"دانه چیا",
+    },
+    {
+        fdc_id:170567,
+        en_name:"almond",
+        fa_name:"بادام درختی",
+    },
+    {
+        fdc_id:1999629,
+        en_name:"Mushrooms, white button",
+        fa_name:"قارچ دکمه ای",
+    },
+    {
+        fdc_id:747447,
+        en_name:"Broccoli",
+        fa_name:"کلم بروکلی",
+    },
+    {
+        fdc_id:169640,
+        en_name:"honey",
+        fa_name:"عسل",
+    },
+    {
+        fdc_id: 748967,
+        en_name: "Eggs, egg whole",
+        fa_name: "تخم مرغ خام بزرگ",
+    },
+    {
+        fdc_id: 747997,
+        en_name: "Eggs, egg white",
+        fa_name: "سفیده تخم مرغ",
+    },
+    {
+        fdc_id: 23377,
+        en_name: "Beef, loin, tenderloin roast",
+        fa_name: "فیله گوشت خالص پخته شده",
+    },
+    {
+        fdc_id: 746785,
+        en_name: "Turkey, ground, pan-broiled",
+        fa_name: "گوشت بوقلمون چرخشده تفتی",
+    },
+    {
+        fdc_id: 331960,
+        en_name: "Chicken, broiler or fryers",
+        fa_name: "فیله مرغ پخته شده",
+    },
+    {
+        fdc_id: 2646171,
+        en_name: "Chicken, thigh",
+        fa_name: "ران مرغ خام",
+    },
+    {
+        fdc_id: 746772,
+        en_name: "Milk, low-fat 1% milk-fat",
+        fa_name: "شیر کم چرب",
+    },
+    {
+        fdc_id: 746776,
+        en_name: "Milk, nonfat",
+        fa_name: "شیر بدون چربی",
+    },
+    {
+        fdc_id: 746778,
+        en_name: "Milk, reduced fat, 2% milk-fat",
+        fa_name: "شیر ۲٪ چربی",
+    },
+    {
+        fdc_id: 746782,
+        en_name: "Milk, 3.25% milk-fat",
+        fa_name: "شیر پر چرب",
+    },
+    {
+        fdc_id: 2259793,
+        en_name: "Yogurt",
+        fa_name: "ماست ساده",
+    },
+    {
+        fdc_id: 2259794,
+        en_name: "Yogurt, Greek",
+        fa_name: "ماست یونانی",
+    },
+    {
+        fdc_id: 330458,
+        en_name: "Coconut Oil",
+        fa_name: "روغن نارگیل",
+    },
+    {
+        fdc_id: 748323,
+        en_name: "Corn Oil",
+        fa_name: "روغن ذرت",
+    },
+    {
+        fdc_id: 1750351,
+        en_name: "Olive Oil extra light",
+        fa_name: "روغن زیتون سبک",
+    },
+    {
+        fdc_id: 748608,
+        en_name: "Olive Oil, extra virgin",
+        fa_name: "روغن زیتون",
+    },
+    {
+        fdc_id: 1750349,
+        en_name: "Sunflower Oil",
+        fa_name: "روغن آفتابگردان",
+    },
+    {
+        fdc_id: 1750350,
+        en_name: "Safflower Oil",
+        fa_name: "روغن گلرنگ",
+    },
+    {
+        fdc_id: 748366,
+        en_name: "Soybean Oil",
+        fa_name: "روغن سویا",
+    },
+    {
+        fdc_id: 169869,
+        en_name: "Peanut butter",
+        fa_name: "کره بادام زمینی کم نمک",
+    },
+]
