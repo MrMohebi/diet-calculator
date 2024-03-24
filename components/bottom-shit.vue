760e0475da3aff9af846f65f41f4927c2e41a130
MMMohebi <MMMohebi@outlook.com>
2024-03-24 10:44:48 +0330

    top sheet

--- components/bottom-shit.vue
+++ components/bottom-shit.vue
@@ -1,6 +1,10 @@
 <template>
-  <div class="bottom-shit" :style="{maxHeight: model?'fit-content':'120px'}">
-    <Button :icon="'pi '+ (model ? ' pi-angle-down ':' pi-angle-up ')" text rounded aria-label="extend" @click="model = !model"/>
+<!--  <div class="bottom-shit" :style="{maxHeight: model?'fit-content':'120px'}">-->
+  <div :class="{'top-shit':topShit, 'bottom-shit': !topShit, 'open':model, 'close':!model}">
+    <div class="row" style="align-items: center;">
+      <Button style="margin: 0 5px" :icon="'pi '+ (model ^ topShit ? ' pi-angle-down ':' pi-angle-up ')" rounded outlined aria-label="extend" @click="model = !model"/>
+      <slot name="header"/>
+    </div>
     <slot/>
   </div>
 </template>
@@ -8,6 +12,10 @@
 <script setup lang="ts">
 const props = defineProps({
   modelValue: Boolean,
+  topShit: {
+    type: Boolean,
+    default:false
+  },
 })
 const emit = defineEmits(["update:modelValue"]);
 
@@ -23,7 +31,27 @@ const model = computed({
 </script>
 
 <style scoped lang="scss">
+.open:not(.top-shit){
+  max-height: 100%;
+}
+
+.close:not(.top-shit){
+  max-height: 15%;
+}
+
+.open:not(.bottom-shit){
+  -ms-transform: translate(0, 0);
+  transform: translate(0, 0);
+}
+
+.close:not(.bottom-shit){
+  -ms-transform: translate(0, calc(-100% + 50px));
+  transform: translate(0, calc(-100% + 50px))
+}
+
 .bottom-shit{
+  display: flex;
+  flex-direction: column;
   width:100vw;
   padding: 5px 15px 5px 15px;
   position: fixed;
@@ -31,5 +59,17 @@ const model = computed({
   left: 0;
   background: white;
   z-index: 999
+}
+.top-shit{
+  display: flex;
+  flex-direction: column-reverse;
+  width:100vw;
+  padding: 5px 15px 5px 15px;
+  position: fixed;
+  top: 0;
+  left: 0;
+  background: white;
+  z-index: 999;
+
 }
 </style>
